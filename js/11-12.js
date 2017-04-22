d9acb215dfb55ba0bcda0d250f30bdc7da7a8b0d
Sergii <ksmua85@gmail.com>
2017-04-22 19:37:16 +0300

    ok

--- js/11-12.js
+++ js/11-12.js
@@ -1,9 +1,14 @@
 $(function(){
 	var html = $('#tmp').html();
-	// console.log('html', html);
+	console.log('html', html);
 	var data = {
-	 	test: 'Hello gays',
-	 	test2: 'test2'
+	 	li1: '<a href="https://www.youtube.com/watch?v=evE4SpLRl78">Я в АйТи хочу пойти<a>',
+	 	li2: 'Я в АйТи хочу пойти',
+	 	li3: 'Может к тридцати пяти',
+	 	li4: 'Я тогда войду в АйТи',
+	 	phone: '+380962139157',
+	 	feedback: 'Якщо потрібно, можу працювати, можу "не працювати"(але подвійна оплата)',
+	 	href: '"https://www.facebook.com/profile.php?id=100002572048895"'
 	};
 	// console.log('data', data);
 	var content = tmpl(html, data);
